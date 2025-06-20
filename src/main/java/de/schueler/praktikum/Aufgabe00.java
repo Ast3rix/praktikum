8a231f253e859620698121710a64f7fcf32e083b
Rudolf Bartsch <bartsch@MacBookPro.fritz.box>
2025-06-20 16:56:39 +0200

    added Task

---
+++ src/main/java/de/schueler/praktikum/Aufgabe00.java
@@ -0,0 +1,12 @@
+package de.schueler.praktikum;
+
+/**
+ * Schreibe ein Programm, das „Hallo Welt!“ auf der Konsole ausgibt.
+ * Google, wie man das in Java macht.
+ */
+public class Aufgabe00 {
+
+    public static void main(String[] args) {
+
+    }
+}
