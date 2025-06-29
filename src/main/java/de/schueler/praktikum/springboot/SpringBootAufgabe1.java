c1c341abd0f4893bfaf572fbbee4652a2b24c087
Rudolf Bartsch <rudolf-bartsch@gmx.de>
2025-06-29 14:16:12 +0200

    Added bruno settings

--- src/main/java/de/schueler/praktikum/springboot/SpringBootAufgabe1.java
+++ src/main/java/de/schueler/praktikum/springboot/SpringBootAufgabe1.java
@@ -7,6 +7,7 @@ package de.schueler.praktikum.springboot;
  *
  * Implementiere den Controller im "springboot" - pacakge. Verwende dazu SpringBoot.
  * Teste den Controller einem Browser z.B. Chrome.
+ * Oder mit "Bruno" (bereits installiert und unter /src/test/resources/bruno eingecheckt)
  *
  */
 public class SpringBootAufgabe1 {
---
+++ src/main/java/de/schueler/praktikum/springboot/SpringBootAufgabe2.java
@@ -0,0 +1,18 @@
+package de.schueler.praktikum.springboot;
+
+/**
+ * Implementiere einen WEB-Endpoint (Controller) mit Springboot, der folgendes kann:
+ * - POST /hello gibt "Hello {vorname} {name}!" zurück
+ * - Die Daten (Vor- und Nachname) sollen aber im Post-Body, in einem JSON, übergeben werden.
+ *      Beispiel:
+ *      {
+ *          "nachname": "Bartsch",
+ *          "vorname": "Rudolf"
+ *      }
+ *
+ * Teste den Controller mit "Bruno".
+ *
+ */
+public class SpringBootAufgabe2 {
+
+}
